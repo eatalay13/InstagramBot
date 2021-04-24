09a1e9c03f35ac69802b38b9287ea53851a35d53
Emrah Atalay <emrahatalay92@gmail.com>
2021-04-24 23:41:35 +0300

    AddAutoMapper

--- BlazorUI/Server/Startup.cs
+++ BlazorUI/Server/Startup.cs
@@ -25,6 +25,8 @@ namespace BlazorUI.Server
 
             services.AddControllersWithViews();
             services.AddRazorPages();
+
+            services.AddAutoMapper(typeof(Startup));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
